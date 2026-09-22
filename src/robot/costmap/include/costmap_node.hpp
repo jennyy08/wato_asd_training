c3300a17da93816f83e7cb0be15887a1469068e7
jennyy08 <jyuan042508@gmail.com>
2026-09-22 00:27:50 -0400

    subscribe to lidar scans

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -2,15 +2,23 @@
 #define COSTMAP_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/string.hpp"
 #include "costmap_core.hpp"
 
 class CostmapNode : public rclcpp::Node {
-  public:
-    CostmapNode();
+ public:
+  CostmapNode();
 
-  private:
-    robot::CostmapCore costmap_;
+  void publishMessage();
+  void lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+
+ private:
+  robot::CostmapCore costmap_;
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
-#endif 
+#endif
